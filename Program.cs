c68bd7579f344843f0396c206383530c319f7d02
Taylor Wirtz <taylor.wirtz@gmail.com>
2021-05-01 17:44:58 -0700

    init commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Device.Gpio;
+using System.Threading;
+
+
+namespace PiBlinkTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Blinking");
+            int pin = 18;
+            using var controller = new GpioController();
+            controller.OpenPin(pin, PinMode.Output);
+            bool ledOn = true;
+
+            while (true)
+            {
+                //controller.Write(pin, ((ledOn) ? PinValue.High : PinValue.Low));
+                Thread.Sleep(1000);
+                ledOn = !ledOn;
+            }
+        }
+    }
+}
+
+
+
+
